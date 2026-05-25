15471ce39e4f649ffdbcf3f97fd9597f65b629a0
kcgiweb <kcgiwebsites@fmail.com>
2026-05-25 17:52:00 +0530

    fix: added htaccess and proxy.php for hostinger

--- test_supabase.js
+++ test_supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pegrruoagqzlxijkafkf.supabase.co';
+const supabaseUrl = 'https://pegrruoagqzlxijkafkr.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlZ3JydW9hZ3F6bHhpamthZmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2OTY4MDYsImV4cCI6MjA5NTI3MjgwNn0.fCOc-nXWzczKrdpYw0TEdki0rezH-PwgmbEyOi0qgv8';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
